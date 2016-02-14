6242852bc2ee4d6899d2ce107bb6fb66db41514b
antoinfive <antoincampbell@live.com>
2016-02-14 03:40:03 -0500

    ember connection made

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214080132) do
+ActiveRecord::Schema.define(version: 20160214081121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20160214080132) do
     t.integer  "player1_score"
     t.integer  "player2_score"
     t.integer  "tournament_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.boolean  "completed",     default: false
   end
 
   add_index "matches", ["tournament_id"], name: "index_matches_on_tournament_id", using: :btree
