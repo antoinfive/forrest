ef26f2eefa53530e7c22dc3661e51af6e20133fe
antoinfive <antoincampbell@live.com>
2016-02-14 16:26:14 -0500

    serving data with relationships in JSON

--- app/serializers/match_serializer.rb
+++ app/serializers/match_serializer.rb
@@ -1,3 +1,3 @@
 class MatchSerializer < ActiveModel::Serializer
-  attributes :id, :player1, :player2, :player1_score, :player2_score, :completed
+  attributes :id, :player1, :player2, :player1_score, :player2_score, :completed, :tournament, :users
 end
